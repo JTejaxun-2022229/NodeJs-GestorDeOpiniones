ea2224042f2d781129519aff995f02bb3e08afeb
jtejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-02-28 15:54:52 -0600

    se agregan archivos y arranque del proyecto

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import { dbConnection } from './mongo';
+import { dbConnection } from './mongo.js';
 
 class Server{
     constructor(){
